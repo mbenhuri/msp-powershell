5560079816a057de094133560a6f933535afd11e
mbenhuri <37915624+mbenhuri@users.noreply.github.com>
2022-10-05 16:50:01 -0700

    Delete customattribute-report (1).ps1

--- customattribute-report (1).ps1
+++
@@ -1,5 +0,0 @@
-$name = Read-Host "What is the domain name?"
-
-Write-Host "Creating the custom attribute report for $name"
-Get-Mailbox -OrganizationalUnit $name | select name, CustomAttribute1 | ft > c:\temp\customattribute-$name.txt
-Write-host "Done. The report is in c:\temp\customattribute-$name.txt"
