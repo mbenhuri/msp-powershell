db9930a39e23096c2d6d27c045cd6ec8a951ce8a
mbenhuri <37915624+mbenhuri@users.noreply.github.com>
2022-10-22 05:01:00 -0700

    Create get user authmethod phone.ps1

---
+++ get user authmethod phone.ps1
@@ -0,0 +1,19 @@
+﻿# Needs the following modules:
+# Install-module Microsoft.Graph.Identity.Signins
+# Assumes a list of upns as csv with heading upn. I previously had this but can connect to azure ad and pull. 
+
+Connect-MgGraph -Scopes UserAuthenticationMethod.ReadWrite.All
+$users = Import-Csv C:\temp\users.csv
+
+$authmethods = @{}
+
+foreach ($user in $users){
+$phone = Get-MgUserAuthenticationPhoneMethod -UserId $user.upn
+
+write-host $user.upn
+write-host $phone.PhoneNumber
+
+$authmethods.add($user.upn,$phone.PhoneNumber)
+}
+
+[PSCustomObject]$authmethods | Export-Csv -Path .\authentication.csv -NoTypeInformation
