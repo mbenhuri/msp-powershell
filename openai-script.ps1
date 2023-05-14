c1608e4e0717dd6b69fef4b5a2bc74699c001986
mbenhuri <37915624+mbenhuri@users.noreply.github.com>
2023-05-13 21:28:19 -0700

    Update openai-script.ps1

--- openai-script.ps1
+++ openai-script.ps1
@@ -4,17 +4,21 @@ Connect-SPOService -Url https://yourtenantname-admin.sharepoint.com
 # Set the URL of the OneDrive site to search
 $siteUrl = "https://yourtenantname-my.sharepoint.com/personal/user_name_domain_com"
 
-# Set the maximum length of the filename (including the path)
+# Set the maximum length of a file path
 $maxPathLength = 260
 
-# Recursively search the site and identify files or folders with long names
-Get-SPOSite $siteUrl | Get-SPOUser | ForEach-Object {
-    Write-Host "Checking OneDrive site:" $_.LoginName
-    $rootFolder = "/" + $_.UserPrincipalName.Replace("@","_").Replace(".","_") + "/"
-    Get-SPOFolder -SiteRelativeUrl $rootFolder -Recursive | ForEach-Object {
-        $itemPath = $_.ServerRelativeUrl
-        if ($itemPath.Length -gt $maxPathLength) {
-            Write-Host "Long filename found:" $itemPath
-        }
+# Get the user's login name
+$user = Get-SPOUser -Site $siteUrl
+
+# Construct the URL of the user's OneDrive site
+$oneDriveUrl = "$siteUrl/Documents"
+
+# Get all files and folders in the user's OneDrive site
+$items = Get-SPOFolderItem -FolderSiteRelativeUrl "/Documents" -Recursive -ItemType All -ErrorAction SilentlyContinue
+
+# Loop through each item in the OneDrive site and output any paths that are too long
+foreach ($item in $items) {
+    if ($item.ServerRelativeUrl.Length -gt $maxPathLength) {
+        Write-Host $item.ServerRelativeUrl
     }
 }
