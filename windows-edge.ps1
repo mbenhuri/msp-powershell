45084b706247699c603ccd73cc113ef48aefca87
mbenhuri <37915624+mbenhuri@users.noreply.github.com>
2024-01-18 18:04:27 -0800

    Update and rename edge.ps1 to windows-edge.ps1

--- windows-edge.ps1
+++ windows-edge.ps1
@@ -1,4 +1,4 @@
+# For the times I end up on a 2012 server and IE will no longer operate
+
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest -uri 'https://go.microsoft.com/fwlink/?linkid=2109047&Channel=Stable&language=en' -OutFile c:\temp\SetupEdge.exe
-
-
