91945b8a175c6332de0d62bf6589412021c807f2
mbenhuri <37915624+mbenhuri@users.noreply.github.com>
2024-01-18 17:45:04 -0800

    Update and rename collect-devicehash to intune-autopilot-collect-devicehash.bat

--- intune-autopilot-collect-devicehash.bat
+++ intune-autopilot-collect-devicehash.bat
@@ -1,2 +1,5 @@
+# This runs the mdm diagnostics tool and extracts the only csv
+# the csv file contains the device hash and can be used to enroll to autopilot
+
 mdmdiagnosticstool.exe -area "DeviceEnrollment;DeviceProvisioning;Autopilot" -zip "c:\temp\MDMDiagReport.zip"
 tar -xvzf c:\temp\MDMDiagReport.zip *.csv
