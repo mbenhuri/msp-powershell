22e0b1bcce4179f8ab0d9132d1d85321ef39f43e
mbenhuri <37915624+mbenhuri@users.noreply.github.com>
2024-01-18 17:55:06 -0800

    Update and rename inboxrulereport.ps1 to ms354-exchange-inboxrulereport.ps1

--- ms354-exchange-inboxrulereport.ps1
+++ ms354-exchange-inboxrulereport.ps1
@@ -1,4 +1,5 @@
-import-module exchangeonline
-connect-exchangeonline
+# quickly gets all the mailboxes and dumps their rules in order to a text file for a review after a breach.
+# no bs
+
 $mailboxes=get-mailbox
 foreach ($mailbox in $mailboxes){get-inboxrule -mailbox $mailbox.alias | fl identity, description | out-file c:\temp\rules.txt -Append}
