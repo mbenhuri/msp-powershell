fa5ca46b4496cf1ecf1416e064b79bd0154630fc
mbenhuri <37915624+mbenhuri@users.noreply.github.com>
2024-01-18 17:51:07 -0800

    Update and rename customattribute-report.ps1 to legacy-customattribute-report.ps1

--- legacy-customattribute-report.ps1
+++ legacy-customattribute-report.ps1
@@ -1,3 +1,6 @@
+# This was for the legacy bit by bit hosted exchange to audit an OU to see if anyone had the wrong custom attribute
+# we used customattribute1 to segment the gal
+
 $name = Read-Host "What is the domain name?"
 
 Write-Host "Creating the custom attribute report for $name"
