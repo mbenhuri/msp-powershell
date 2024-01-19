0cbc05d0e6f51e8acc48f96c99364590d685f1f1
mbenhuri <37915624+mbenhuri@users.noreply.github.com>
2024-01-18 17:54:05 -0800

    Update and rename list-distributiongroupmembers.ps1 to legacy-list-distributiongroupmembers.ps1

--- legacy-list-distributiongroupmembers.ps1
+++ legacy-list-distributiongroupmembers.ps1
@@ -1,3 +1,5 @@
+# One of a few methods I devised to list all group members in a digestible file for people without access to AD
+
 ﻿$saveto = "C:\temp\listmembers.txt"
 
 Get-DistributionGroup -OrganizationalUnit exampledomain.com | sort name | ForEach-Object {
