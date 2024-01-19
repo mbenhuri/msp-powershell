eef2c54c1648c2203f5db430b85d90255a7b8c53
mbenhuri <37915624+mbenhuri@users.noreply.github.com>
2024-01-18 17:53:17 -0800

    Update and rename onprem-Groupmembers.ps1 to legacy-onprem-Groupmembers.ps1

--- legacy-onprem-Groupmembers.ps1
+++ legacy-onprem-Groupmembers.ps1
@@ -1,3 +1,6 @@
+# For legacy exchange environments this will list out and export all the members of distribution lists to a csv
+# I would then make a quick pivot table to review with clients
+
 $Groups = Get-ADGroup -Filter {GroupScope -eq 'Global' -and Members -ne "NULL"}
 $Users = foreach( $Group in $Groups ){
     Get-ADGroupMember -Identity $Group | foreach {
